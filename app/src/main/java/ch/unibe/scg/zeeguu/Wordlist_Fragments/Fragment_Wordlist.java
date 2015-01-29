6a0b5fa42f479a21e8f437449731136eff81fca9
Pascal Giehl <p.giehl@gmx.ch>
2015-01-29 15:18:22 +0100

    Add text when wordlist empty

--- app/src/main/java/ch/unibe/scg/zeeguu/Wordlist_Fragments/Fragment_Wordlist.java
+++ app/src/main/java/ch/unibe/scg/zeeguu/Wordlist_Fragments/Fragment_Wordlist.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -37,9 +38,12 @@ public class Fragment_Wordlist extends ZeeguuFragment {
 
         //create listview for wordlist and customize it
         adapter = new WordlistAdapter(this.getActivity(), list);
-
         ListView resultList = (ListView) view.findViewById(R.id.wordlist_listview);
         resultList.setAdapter(adapter);
+
+        //Set text when listview empty
+        TextView emptyText = (TextView) view.findViewById(R.id.wordlist_empty);
+        resultList.setEmptyView(emptyText);
     }
 
     @Override
