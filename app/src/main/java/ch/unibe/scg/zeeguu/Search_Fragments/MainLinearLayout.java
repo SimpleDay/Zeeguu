74bfadf01ff428abbd258f6e44bf57315ea030f1
Pascal Giehl <p.giehl@gmx.ch>
2015-04-29 11:41:00 +0200

    Responsive keyboard layout

---
+++ app/src/main/java/ch/unibe/scg/zeeguu/Search_Fragments/MainLinearLayout.java
@@ -0,0 +1,37 @@
+package ch.unibe.scg.zeeguu.Search_Fragments;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.LinearLayout;
+
+import ch.unibe.scg.zeeguu.R;
+
+/**
+ * Zeeguu Application
+ * Created by Pascal on 29/04/15.
+ */
+public class MainLinearLayout extends LinearLayout {
+
+    public MainLinearLayout(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+    }
+
+    //// little hack to be able to integrate a keyboard listener ////
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Log.d("Search Layout", "Handling Keyboard Window shown");
+
+        final int proposedheight = MeasureSpec.getSize(heightMeasureSpec);
+        final int actualHeight = getHeight();
+
+        if (actualHeight > proposedheight){
+            //when keyboard is closed
+            findViewById(R.id.relativeLayout_text_translated).setVisibility(GONE);
+        } else {
+            //when keyboard is opened
+            findViewById(R.id.relativeLayout_text_translated).setVisibility(VISIBLE);
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
