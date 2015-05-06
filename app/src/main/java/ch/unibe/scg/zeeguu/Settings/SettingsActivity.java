01300899ba6a35baab21f9b251905e13f28aa5a8
Pascal Giehl <p.giehl@gmx.ch>
2015-05-06 17:01:02 +0200

    Backbutton of SettingsActivity, not yet working

--- app/src/main/java/ch/unibe/scg/zeeguu/Settings/SettingsActivity.java
+++ app/src/main/java/ch/unibe/scg/zeeguu/Settings/SettingsActivity.java
@@ -1,6 +1,7 @@
 package ch.unibe.scg.zeeguu.Settings;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 
 public class SettingsActivity extends ActionBarActivity {
@@ -9,9 +10,15 @@ public class SettingsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //show back button
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true); //TODO: not yet working the backbutton
+        // actionBar.setIcon(R.drawable.ic_launcher); show elephant icon instead
+
         // Display the fragment as the main content.
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new FragmentSettings())
                 .commit();
     }
+
 }
