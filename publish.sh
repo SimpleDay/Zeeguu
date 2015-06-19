b03a555d422266be2f4c2639164fdafc8328524c
Mircea Lungu <mircea.lungu@gmail.com>
2015-06-19 09:18:51 +0200

    script to automate publishing a new .apk

---
+++ publish.sh
@@ -0,0 +1,3 @@
+gradlew assembleRelease
+open app/build/outputs/apk/
+open "https://accounts.google.com/AccountChooser?continue=https%3A%2F%2Fplay.google.com%2Fapps%2Fpublish&hl=en&service=androiddeveloper"
